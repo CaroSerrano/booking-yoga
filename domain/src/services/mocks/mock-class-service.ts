d09a740ebaad61b0437c3006e34884a2b9b887e7
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-10-16 11:18:00 -0300

    add mocked class service

---
+++ domain/src/services/mocks/mock-class-service.ts
@@ -0,0 +1,42 @@
+import type { Class } from '../../entities/class.js';
+import type { ClassService } from '../class-service.js';
+
+export class MockedClassService implements ClassService {
+  classes: Class[] = [];
+
+  constructor(classes: Class[]) {
+    this.classes = classes;
+  }
+
+  updateOne = async (id: string, data: Partial<Class>) => {
+    const index = this.classes.findIndex((c) => c.id === id);
+    if (index === -1) return undefined;
+
+    this.classes[index] = { ...this.classes[index], ...data };
+    return this.classes[index];
+  };
+  findAll = async () => {
+    return this.classes;
+  };
+  findById = async (id: string) => {
+    return this.classes.find((c) => c.id == id);
+  };
+  save = async (data: Class) => {
+    this.classes.push(data);
+  };
+  findByStartDate = async (date: Date) => {
+    return this.classes.filter((c) => c.start == date);
+  };
+  findByLocation = async (location: string) => {
+    return this.classes.filter((c) => c.location === location);
+  };
+  findByTitle = async (title: string) => {
+    return this.classes.filter((c) => c.title === title);
+  };
+  findByTeacher = async (teacherId: string) => {
+    return this.classes.filter((c) => c.teacher === teacherId);
+  };
+  delete = async (id: string) => {
+    this.classes = this.classes.filter((c) => c.id !== id);
+  };
+}

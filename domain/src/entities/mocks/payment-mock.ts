4339ec599aab8b2a9338504450efb0e80ef845f3
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-10-18 17:04:52 -0300

    add Payment mock

---
+++ domain/src/entities/mocks/payment-mock.ts
@@ -0,0 +1,16 @@
+import { faker } from '@faker-js/faker';
+import generateTimestamps from '../../utils/generateTimestamps';
+import { Payment, PaymentStatus } from '../payment';
+
+export function paymentMock(opts?: Partial<Payment>): Payment {
+  return {
+    id: faker.string.uuid(),
+    bookingId: faker.string.uuid(),
+    userId: faker.string.uuid(),
+    amount: Number(faker.commerce.price()),
+    currency: faker.finance.currency().code,
+    status: PaymentStatus.PENDING,
+    ...generateTimestamps(),
+    ...opts,
+  };
+}

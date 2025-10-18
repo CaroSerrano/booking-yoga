190995ea9754d07885d1ce0fdaee8971e7b10f13
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-10-18 16:59:51 -0300

    add mocked payment service

---
+++ domain/src/services/mocks/mock-payment-service.ts
@@ -0,0 +1,36 @@
+import type { Payment } from '../../entities/payment.js';
+import type { PaymentService } from '../payment-service.js';
+
+export class MockedPaymentService implements PaymentService {
+  payments: Payment[] = [];
+
+  constructor(payments: Payment[]) {
+    this.payments = payments;
+  }
+
+  updateOne = async (id: string, data: Partial<Payment>) => {
+    const index = this.payments.findIndex((p) => p.id === id);
+    if (index === -1) return undefined;
+
+    this.payments[index] = { ...this.payments[index], ...data };
+    return this.payments[index];
+  };
+  findAll = async () => {
+    return this.payments;
+  };
+  findById = async (id: string) => {
+    return this.payments.find((p) => p.id == id);
+  };
+  findByBookingId = async (bookingId: string) => {
+    return this.payments.filter((p) => p.bookingId == bookingId);
+  };
+  findByUserId = async (userId: string) => {
+    return this.payments.filter((b) => b.userId == userId);
+  };
+  save = async (data: Payment) => {
+    this.payments.push(data);
+  };
+  delete = async (id: string) => {
+    this.payments = this.payments.filter((p) => p.id !== id);
+  };
+}

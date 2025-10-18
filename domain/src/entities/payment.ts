71afe7a830ef2c00a330ea36ef4a809d0325a6c9
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-10-18 16:54:23 -0300

    typo: uppercase in type name

--- domain/src/entities/payment.ts
+++ domain/src/entities/payment.ts
@@ -1,12 +1,12 @@
 import { Entity } from '../utils/types/entity';
 
-export const paymentStatus = {
+export const PaymentStatus = {
   PENDING: 'PENDING',
   COMPLETED: 'COMPLETED',
   FAILED: 'FAILED',
 } as const;
 
-export type PaymentStatus = (typeof paymentStatus)[keyof typeof paymentStatus];
+export type PaymentStatus = (typeof PaymentStatus)[keyof typeof PaymentStatus];
 
 export interface Payment extends Entity {
   userId: string;

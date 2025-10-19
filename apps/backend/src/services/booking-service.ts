1cb95491151db2bb4f2f5c0d22ebf8b1d2734354
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-10-19 16:46:22 -0300

    add booking service implementation

---
+++ apps/backend/src/services/booking-service.ts
@@ -0,0 +1,53 @@
+import { PrismaClient } from 'src/generated/prisma/index.js';
+import type { Booking, BookingService } from 'booking-domain';
+
+export class BookingServiceImplementation implements BookingService {
+  prisma: PrismaClient;
+
+  constructor(prisma: PrismaClient) {
+    this.prisma = prisma;
+  }
+
+  async findAll() {
+    return this.prisma.booking.findMany();
+  }
+
+  async findById(id: string) {
+    const classFound = await this.prisma.booking.findUnique({ where: { id } });
+    return classFound ?? undefined;
+  }
+
+  async save(data: Booking) {
+    this.prisma.booking.create({ data });
+  }
+
+  async updateOne(id: string, data: Partial<Booking>) {
+    const booking = await this.prisma.booking.update({
+      where: { id },
+      data,
+    });
+    return booking ?? undefined;
+  }
+
+  async delete(id: string) {
+    this.prisma.booking.delete({ where: { id } });
+  }
+
+  async findByClassId(classId: string) {
+    return this.prisma.booking.findMany({ where: { classId } });
+  }
+
+  async findByUserId(userId: string) {
+    return this.prisma.booking.findMany({ where: { userId } });
+  }
+
+  async findByUserAndClass(classId: string, userId: string) {
+    const booking = await this.prisma.booking.findFirst({
+      where: {
+        classId,
+        userId,
+      },
+    });
+    return booking ?? undefined;
+  }
+}

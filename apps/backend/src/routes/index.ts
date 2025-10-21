358ea93aad0ebab2b07a5536847affbef16eb9b7
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-10-21 15:19:49 -0300

    add payment router

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -4,6 +4,7 @@ import authRouter from './auth-router.js';
 import userRouter from './user-router.js';
 import classRouter from './class-router.js';
 import bookingRouter from './booking-router.js';
+import paymentRouter from './payment-router.js';
 
 const apiRouter = (app: Express) => {
   const router = express.Router();
@@ -11,6 +12,7 @@ const apiRouter = (app: Express) => {
   router.use('/api/user', userRouter);
   router.use('/api/class', classRouter);
   router.use('/api/booking', bookingRouter);
+  router.use('/api/payment', paymentRouter);
   app.use(router);
 };
 
---
+++ apps/backend/src/routes/payment-router.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import { paymentController } from 'src/controllers/payment-controller.js';
+import {
+  paymentService,
+  userService,
+  bookingService,
+  classService,
+} from 'src/services/index.js';
+
+const controller = paymentController({
+  paymentService,
+  userService,
+  bookingService,
+  classService,
+});
+
+const router = express.Router();
+
+router.get('/', controller.listUserPayments);
+router.get('/bookingId', controller.findBookingPayment);
+router.post('/', controller.createPayment);
+router.patch('/:id', controller.updatePayment);
+
+export default router;

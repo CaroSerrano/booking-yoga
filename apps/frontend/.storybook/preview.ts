8e72921ef4f0402cf06ebe14ef72d18c24f29166
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-10-24 12:28:56 -0300

    add tailwind css

--- apps/frontend/.storybook/preview.ts
+++ apps/frontend/.storybook/preview.ts
@@ -1,13 +1,21 @@
-import type { Preview } from '@storybook/react-vite'
+import type { Preview } from '@storybook/react-vite';
+import '../src/index.css';
+
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
+    backgrounds: {
+      options: {
+        dark: { name: 'Dark', value: '#19142e' },
+        light: { name: 'Light', value: '#ffffff' },
+      }
+    }
   },
 };
 

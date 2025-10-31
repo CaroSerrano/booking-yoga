6f1cca1029affccc07b7876105a7c0016e36445b
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-10-31 10:24:39 -0300

    update to include admin view

--- apps/frontend/src/components/Calendar.tsx
+++ apps/frontend/src/components/Calendar.tsx
@@ -2,14 +2,49 @@ import FullCalendar from '@fullcalendar/react';
 import type { EventInput } from '@fullcalendar/core/index.js';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import listPlugin from '@fullcalendar/list';
+import { useEffect, useState } from 'react';
+import { Modal } from './Modal';
+import type { UserResponseDTO } from 'booking-backend';
+import { AdminClassDetails } from './AdminClassDetails';
+import type { ClassDetails } from './AdminClassDetails';
 
 export interface CalendarProps {
   events: EventInput[];
+  isAdmin?: boolean;
+  fetchClasses: () => Promise<void>;
 }
 
-export default function Calendar({ events }: CalendarProps) {
+export default function Calendar({ events, isAdmin = false, fetchClasses }: CalendarProps) {
+  const [openAdminModal, setOpenAdminModal] = useState(false);
+  const [openUserModal, setOpenUserModal] = useState(false);
+  const [currentEvent, setCurrentEvent] = useState<ClassDetails | null>(null);
+  const [teachers, setTeachers] = useState<UserResponseDTO[]>([]);
+
+  useEffect(() => {
+    const fetchTeachers = async () => {
+      try {
+        const res = await fetch('http://localhost:3000/api/user/teachers', {
+          credentials: 'include',
+        });
+        if (res.ok) {
+          const data = await res.json();
+          setTeachers(data);
+        }
+      } catch {
+        setTeachers([]);
+      }
+    };
+    fetchTeachers();
+  }, []);
+
+  const onAdminEventClick = () => {
+    setOpenAdminModal(true);
+  };
+  const onUserEventClick = () => {
+    setOpenUserModal(true);
+  };
   return (
-    <div className='w-[90%] h-fit p-5 m-5 bg-black/90 rounded-2xl'>
+    <div className='w-[90%] h-fit p-5 m-5 bg-zinc-950/90 rounded-2xl'>
       <FullCalendar
         plugins={[dayGridPlugin, listPlugin]}
         initialView='dayGridMonth'
@@ -27,10 +62,38 @@ export default function Calendar({ events }: CalendarProps) {
           }
         }}
         eventClick={(info) => {
-          alert(`evento clickeado:  ${info.event.title}`);
+          const eventObject = {
+            start: info.event._instance?.range.start,
+            end: info.event._instance?.range.end,
+            ...info.event._def,
+          };
+          setCurrentEvent(eventObject);
+          if (isAdmin) {
+            onAdminEventClick();
+          } else {
+            onUserEventClick();
+          }
         }}
         eventClassNames='cursor-pointer'
       />
+      {openAdminModal && (
+        <Modal
+          isOpen={openAdminModal}
+          onClose={() => setOpenAdminModal(false)}
+          title='Class details'
+        >
+          <AdminClassDetails currentClass={currentEvent} teachers={teachers} fetchClasses={fetchClasses} />
+        </Modal>
+      )}
+      {openUserModal && (
+        <Modal
+          isOpen={openUserModal}
+          onClose={() => setOpenUserModal(false)}
+          title='Class details'
+        >
+          User modal content
+        </Modal>
+      )}
     </div>
   );
 }

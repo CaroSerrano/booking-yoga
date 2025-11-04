f5ef1715576b1a37cecb4eafddddc5f036295529
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-11-04 11:08:14 -0300

    delete unused props

--- apps/frontend/src/pages/Login.tsx
+++ apps/frontend/src/pages/Login.tsx
@@ -4,20 +4,16 @@ import type { LoginFormProps } from '../components/LoginForm';
 export function Login({
   onSubmit,
   defaultValues,
-  error,
   loading,
   showPassword,
-  successMessage,
 }: LoginFormProps) {
   return (
     <article className='flex flex-col items-center'>
       <LoginForm
         onSubmit={onSubmit}
         defaultValues={defaultValues}
-        error={error}
         loading={loading}
         showPassword={showPassword}
-        successMessage={successMessage}
       />
     </article>
   );

aace578e132592c34c440835331a91d1c7f55113
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-11-10 12:57:54 -0300

    update user type

--- apps/frontend/src/components/Header.tsx
+++ apps/frontend/src/components/Header.tsx
@@ -1,15 +1,10 @@
-import type { Role, UserStatus } from 'booking-domain';
+import { Link } from 'react-router-dom';
 import { Button } from './Button';
+import type { UserResponseDTO } from 'booking-backend';
 
-export type User = {
-  name: string;
-  email: string;
-  role: Role;
-  status: UserStatus;
-};
 
 export interface HeaderProps {
-  user?: User;
+  user?: UserResponseDTO;
   onLogin?: () => void;
   onLogout?: () => void;
   onCreateAccount?: () => void;
@@ -22,24 +17,26 @@ export const Header = ({
   onCreateAccount,
 }: HeaderProps) => (
   <header className='w-full'>
-    <div className='w-full flex justify-between items-center border-b border-white px-5 py-4'>
+    <div className='w-full flex justify-between items-center border-b border-white px-2 md:px-5 py-4'>
       <div className='flex gap-2 items-center'>
+        <Link to='/'>
         <img
           src='/logo.png'
           alt='Logo'
-          width='90'
-          height='90'
+          width='80'
+          height='80'
           className='mx-auto rounded-full bg-white p-2 shadow-md backdrop-blur-sm justify-self-center'
         />
-        <h1>Yoga Studio</h1>
+        </Link>
+        <h1 className='hidden md:flex'>Yoga Studio</h1>
       </div>
-      <div className='flex gap-2 items-center'>
+      <div className='flex gap-1 md:gap-2 items-center'>
         {user ? (
           <>
             <span>
               Welcome, <b>{user.name}</b>!
             </span>
-            <Button onClick={onLogout}>Log out</Button>
+            <Button className='text-sm md:text-md text-nowrap' onClick={onLogout}>Log out</Button>
           </>
         ) : (
           <>
